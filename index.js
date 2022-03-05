cfeae251208c8a648cbce139a83b36eda4bcfbd7
Philip Krause <pkrause@amazon-5415886c3.localdomain>
2022-03-05 03:27:40 -0600

    Changes unrelated to a JIRA task

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-//5rown 4x the prize
-//4inarow
-
 //Reel Distribution
 //1Jack , 2Queen, 3King, 4Ace, 5Heart, 6Club, 7Diamond, 8Spade
-const reelOne =   [1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 8, 8, 8]
-const reelTwo =   [1, 2, 2, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 8, 8, 8]
-const reelThree = [1, 1, 1, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 8]
-const reelFour =  [1, 1, 1, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 8]
-const reelFive =  [1, 1, 1, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 8]
-
-
+const reel_1 = [1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 8, 8, 8]
+const reel_2 = [1, 2, 2, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 8, 8, 8]
+const reel_3 = [1, 1, 1, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 8]
+const reel_4 = [1, 1, 1, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 8]
+const reel_5 = [1, 1, 1, 1, 1, 1, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 8]
 
 //Payouts 
 const jackPayout = .35
@@ -20,11 +15,12 @@ const heartPayout = 1.25
 const clubPayout = 1.50
 const diamondPayout = 1.75
 const spadePayout = 2
-
 const payOutArray = [jackPayout, queenPayout, kingPayout, acePayout, heartPayout, clubPayout, diamondPayout, spadePayout]
 
 const totalSpins = 100
+const totalReels = 5
 const reelLength = reelOne.length
+let totalCells = reelLength * totalReels
 
 let jackWins = 0
 let queenWins = 0
@@ -35,14 +31,12 @@ let clubWins = 0
 let diamondWins = 0
 let spadeWins = 0
 
-
 let losses = 0
 let wins = 0
 
 let totalBet = 0
 let totalCashWon = 0
 let returnToPlayer
-
 
 const payOutLine = (line) => {
    for (let index = 0; index < payOutArray.length; index++) {
@@ -54,41 +48,82 @@ const payOutLine = (line) => {
    }
 }
 
+const creatRandomNumber = (reelSize) => {
+   let rng = Math.floor(Math.random() * (reelSize - 2))
+   return rng
+}
+
 for (let index = 0; index < totalSpins; index++) {
 
    totalBet++
 
-   rngOne = Math.floor(Math.random() * (reelLength - 2))
-   rngTwo = Math.floor(Math.random() * (reelLength - 2))
-   rngThree = Math.floor(Math.random() * (reelLength - 2))
+   let cell_1 = rng_1 + 4
+   let cell_2 = rng_2 + 4
+   let cell_3 = rng_3 + 4
+   let cell_4 = rng_4 + 4
+   let cell_5 = rng_5 + 4
+   
+   let cell_6 = rng_1 + 3
+   let cell_7 = rng_2 + 3
+   let cell_8 = rng_3 + 3
+   let cell_9 = rng_4 + 3
+   let cell_10 = rng_5 + 3
 
-   let topRow1 = rngOne + 2
-   let topRow2 = rngTwo + 2
-   let topRow3 = rngThree + 2
-   let midRow1 = rngOne + 1
-   let midRow2 = rngTwo + 1
-   let midRow3 = rngThree + 1
-   let botRow1 = rngOne
-   let botRow2 = rngTwo
-   let botRow3 = rngThree
+   let cell_11 = rng_1 + 2
+   let cell_12 = rng_2 + 2
+   let cell_13 = rng_3 + 2
+   let cell_14 = rng_4 + 2
+   let cell_15 = rng_5 + 2
 
-   let cellOne = reelOne[topRow1]
-   let cellTwo = reelTwo[topRow2]
-   let cellThree = reelThree[topRow3]
-   let cellFour = reelOne[midRow1]
-   let cellFive = reelTwo[midRow2]
-   let cellSix = reelThree[midRow3]
-   let cellSeven = reelOne[botRow1]
-   let cellEight = reelTwo[botRow2]
-   let cellNine = reelThree[botRow3]
+   let cell_16 = rng_1 + 1
+   let cell_17 = rng_2 + 1
+   let cell_18 = rng_3 + 1
+   let cell_19 = rng_4 + 1
+   let cell_20 = rng_5 + 1
+
+   let cell_21 = rng_1
+   let cell_22 = rng_2
+   let cell_23 = rng_3
+   let cell_24 = rng_4
+   let cell_25 = rng_5
 
 
-   const Outcome = [cellOne, cellTwo, cellThree, cellFour, cellFive, cellSix, cellSeven, cellEight, cellNine]
-   const lineAcrossOne = [cellOne, cellTwo, cellThree]
-   const lineAcrossTwo = [cellFour, cellFive, cellSix]
-   const lineAcrossThree = [cellSeven, cellEight, cellNine]
-   const lineDiagnalTopDown = [cellOne, cellFive, cellNine]
-   const lineDiagnalDownTop = [cellSeven, cellFive, cellThree]
+   let cell_1_Outcome = reel_1[cell_1]
+   let cell_2_Outcome = reel_2[cell_2]
+   let cell_3_Outcome = reel_3[cell_3]
+   let cell_4_Outcome = reel_4[cell_4]
+   let cell_5_Outcome = reel_5[cell_5]
+   let cell_6_Outcome = reel_1[cell_6]
+   let cell_7_Outcome = reel_2[cell_7]
+   let cell_8_Outcome = reel_3[cell_8]
+   let cell_9_Outcome = reel_4[cell_9]
+   let cell_10_Outcome = reel_5[cell_10]
+   let cell_11_Outcome = reel_1[cell_11]
+   let cell_12_Outcome = reel_2[cell_12]
+   let cell_13_Outcome = reel_3[cell_13]
+   let cell_14_Outcome = reel_4[cell_14]
+   let cell_15_Outcome = reel_5[cell_15]
+   let cell_16_Outcome = reel_1[cell_16]
+   let cell_17_Outcome = reel_2[cell_17]
+   let cell_18_Outcome = reel_3[cell_18]
+   let cell_19_Outcome = reel_4[cell_19]
+   let cell_20_Outcome = reel_5[cell_20]
+   let cell_21_Outcome = reel_1[cell_21]
+   let cell_22_Outcome = reel_2[cell_22]
+   let cell_23_Outcome = reel_3[cell_23]
+   let cell_24_Outcome = reel_4[cell_24]
+   let cell_25_Outcome = reel_5[cell_25]
+
+
+   //const totalOutcome = [cellOne, cellTwo, cellThree, cellFour, cellFive, cellSix, cellSeven, cellEight, cellNine]
+   const lineAcross_1 = [cell_1, cell_2, cell_3, cell_4, cell_5]
+   const lineAcross_2 = [cell_6, cell_7, cell_8, cell_9, cell_10]
+   const lineAcross_3 = [cell_11, cell_12, cell_13, cell_14, cell_15]
+   const lineAcross_4 = [cell_16, cell_17, cell_18, cell_19, cell_20]
+   const lineAcross_5 = [cell_21, cell_22, cell_23, cell_24, cell_25]
+
+   const lineDiagnalTopDown = [cell_21, cell_17, cell_13, cell_9, cell_5]
+   // const lineDiagnalDownTop = [cellSeven, cellFive, cellThree]
 
    //Across Wins
    if(lineAcrossOne[0] == lineAcrossOne[1] && lineAcrossOne[1] == lineAcrossOne[2]){
