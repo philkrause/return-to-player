3574cc215e1309ba3a7dfa1d6c95074b888f119b
Philip Krause <pkrause@amazon-5415886c3.localdomain>
2022-03-04 18:31:46 -0600

    Changes unrelated to a JIRA task

--- index.js
+++ index.js
@@ -1,53 +1,126 @@
+//5rown 4x the prize
+//4inarow
 
 //Reel Distribution
-const reelOne = [1,1,1,2]
-const reelTwo = [1,1,1,2]
+//1Jack , 2Queen, 3King, 4Ace, 5Shield, 6Axe, 7Mace, 8Sword
+const reelOne = [1, 1, 1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 6, 6, 7, 7, 8]
+const reelTwo = [1, 1, 1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 6, 6, 7, 7, 8]
+const reelThr = [1, 1, 1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 6, 6, 7, 7, 8]
 
 //Payouts 
-const jackPayout = 1.5
-const queenPayout = 3
+const jackPayout = .35
+const queenPayout = .45
+const kingPayout = .55
+const acePayout = 1
+const shieldPayout = 1.25
+const axePayout = 1.50
+const macePayout = 1.75
+const swordPayout = 2
 
-const totalSpins = 500
+const totalSpins = 100000
 const reelLength = reelOne.length
+
 let jackWins = 0
 let queenWins = 0
+let kingWins = 0
+let aceWins = 0
+let shieldWins = 0
+let axeWins = 0
+let maceWins = 0
+let swordWins = 0
+
+
 let losses = 0
+let wins = 0
 
 let totalBet = 0
-let totalWin = 0
+let totalCashWon = 0
 let returnToPlayer
 
 for (let index = 0; index < totalSpins; index++) {
 
-   
+
    totalBet++
 
    rngOne = Math.floor(Math.random() * (reelLength))
    rngTwo = Math.floor(Math.random() * (reelLength))
-   
+   rngThree = Math.floor(Math.random() * (reelLength))
+
    reelOneOutcome = reelOne[rngOne]
    reelTwoOutcome = reelTwo[rngTwo]
+   reelThrOutcome = reelThr[rngTwo]
 
-   if(reelOneOutcome === 1 && reelTwoOutcome === 1){
-      totalWin+=jackPayout
+
+   if (reelOneOutcome === 1 && reelTwoOutcome === 1 && reelThrOutcome === 1) {
+      totalCashWon += jackPayout
       jackWins++
-      console.log(`!! Win 1.5$ !!`)
+      console.log(`!! Player Win .30$ !!`)
    }
-   if(reelOneOutcome === 2 && reelTwoOutcome === 2){
-      totalWin+=queenPayout
+
+   if (reelOneOutcome === 2 && reelTwoOutcome === 2 && reelThrOutcome === 2) {
+      totalCashWon += queenPayout
       queenWins++
-      console.log(`!! Win 3$ !!`)
+      console.log(`!! Player Win .45$ !!`)
+   }
+   if (reelOneOutcome === 3 && reelTwoOutcome === 3 && reelThrOutcome === 3) {
+      totalCashWon += kingPayout
+      kingWins++
+      console.log(`!! Player Win .55$ !!`)
+   }
+
+   if (reelOneOutcome === 4 && reelTwoOutcome === 4 && reelThrOutcome === 4) {
+      totalCashWon += acePayout
+      aceWins++
+      console.log(`!! Player Win 1$ !!`)
+   }
+   if (reelOneOutcome === 5 && reelTwoOutcome === 5 && reelThrOutcome === 5) {
+      totalCashWon += shieldPayout
+      shieldWins++
+      console.log(`!! Player Win 1.25$ !!`)
+   }
+   if (reelOneOutcome === 6 && reelTwoOutcome === 6 && reelThrOutcome === 6) {
+      totalCashWon += axePayout
+      axeWins++
+      console.log(`!! Player Win 1.50$ !!`)
+   }
+   if (reelOneOutcome === 7 && reelTwoOutcome === 7 && reelThrOutcome === 7) {
+      totalCashWon += macePayout
+      maceWins++
+      console.log(`!! Player Win 1.50$ !!`)
+   }
+   if (reelOneOutcome === 8 && reelTwoOutcome === 8 && reelThrOutcome === 8) {
+      totalCashWon += swordPayout
+      swordWins++
+      console.log(`!! BIG Win 2$ !!`)
    }
    else {
       losses++
-      console.log("LOSE")
+      console.log("HOUSE WINS")
    }
+
 }
 
-returnToPlayer = 100 * (totalBet/totalWin)
+returnToPlayer = (totalCashWon/totalBet) * 100
 
 
-console.log(`Return to Player: ${returnToPlayer}`)
+
+let stake = totalCashWon - totalBet
+let allWins = jackWins + queenWins + aceWins + shieldWins + axeWins + maceWins + swordWins
+let winRatio = allWins/losses
+
+console.log(`Reel Length: ${reelLength}`)
+console.log(`TotalWin: ${allWins}`)
+console.log(`Total Losses: ${losses}`)
+console.log(`Win Ration: ${winRatio}`)
+console.log(`TotalBet: ${totalBet}`)
+console.log(`Stake: ${stake}`)
 console.log(`JackWins: ${jackWins}`)
 console.log(`QueenWins: ${queenWins}`)
-console.log(`Total Losses: ${losses}`)
+console.log(`KingWins: ${kingWins}`)
+console.log(`AceWins: ${aceWins}`)
+console.log(`ShieldWins: ${shieldWins}`)
+console.log(`AxeWins: ${axeWins}`)
+console.log(`MaceWins: ${maceWins}`)
+console.log(`SwordWins: ${swordWins}`)
+console.log(`Return to Player: ${returnToPlayer}`)
+
